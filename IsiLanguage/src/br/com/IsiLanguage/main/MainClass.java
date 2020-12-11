49538e407423c8a791c2e22243d13c4d24415b25
Daniel Vieira Batista <danielemaileletronico@gmail.com>
2020-12-11 00:25:22 -0300

    adicionando novas instruções e iniciando o analisador léxico e semântico

---
+++ IsiLanguage/src/br/com/IsiLanguage/main/MainClass.java
@@ -0,0 +1,24 @@
+package br.com.IsiLanguage.main;
+
+public class MainClass {
+	
+	/*
+	 * Essa classe é responsável por apontar o arquivo que vai passar pelo compilador
+	 * Que vai invocar o parser, etc...
+	 * 
+	 * 
+	 * Nas palavras do professor: é responsável por criar a interação com o usuário
+	 * instanciando o parser que será criado e apontando para o arquivo fonte
+	 * 
+	 * Arquivo fonte: sempre terá e extensão .isi
+	 * 
+	 * */
+	
+	public static void main(String [] args){
+		
+		
+		
+	}
+	
+
+}
